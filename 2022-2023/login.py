fce62bb4d4f1e03389fc04135ce81042cb8a9365
Roncz <roncz.oliver@students.jedlik.eu>
2022-11-16 11:01:20 +0100

    bejelenkezési rendszer kész #20

---
+++ 2022-2023/login.py
@@ -0,0 +1,49 @@
+import hashlib
+from master import *
+
+def signup():
+    username = input('Írja be a felhasználónevét: ')
+    password = input('Írjon be egy jelszót: ')
+    ConfirmPassword = input('Erősítse meg jelszavát: ')
+    if ConfirmPassword == password:
+        enc = ConfirmPassword.encode()
+        hash1 = hashlib.md5(enc).hexdigest()
+        with open('bejelentkezesi_adatok.txt', 'w') as f:
+            f.write(username + '\n')
+            f.write(hash1)
+        f.close()
+        print('Sikeres regisztráció!\n')
+    else:
+        print('A két jelszó nem egyezik meg.\n')
+        input()
+
+def login():
+    username = input('Felhasználónév: ')
+    password = input('Jelszó: ')
+    authorization = password.encode()
+    AuthorizationHash = hashlib.md5(authorization).hexdigest()
+    with open('bejelentkezesi_adatok.txt', 'r') as f:
+        StoredUsername, StoredPassword = f.read().split('\n')
+    f.close()
+    if username == StoredUsername and AuthorizationHash == StoredPassword:
+        print('Sikeres bejelentkezés!')
+        Master()
+    else:
+        print('Sikertelen bejelentkezés!\n')
+        input()
+         
+while True:
+    print("********** Bejelentkezés **********")
+    print('1..Új fiók létrehozása')
+    print('2..Bejelentkezés')
+    print('3..Kilépés a programból')
+    choice = int(input('Választás: '))
+    if choice == 1:
+        signup()
+    elif choice == 2:
+        login()
+    elif choice == 3:
+        break
+    else:
+        print('192.176.45.34')
+
--- 2022-2023/master.py
+++ 2022-2023/master.py
@@ -1,21 +1,22 @@
 from menu import *
 from data import *
 from functions import *
-ReadFile()
+def Master():
+    ReadFile()
 
-choice = menu()
-while choice != 0:
-    if choice == 1:
-        ViewList(True)
-    if choice == 2:
-        NewWeapon()
-    if choice == 3:
-        licit()
-    if choice == 4:
-        DeleteWeapon()
-    if choice == 5:
-        ModifyWeapon()
-    if choice == 6:
-        ViewShoppingCart()
     choice = menu()
+    while choice != 0:
+        if choice == 1:
+            ViewList(True)
+        if choice == 2:
+            NewWeapon()
+        if choice == 3:
+            licit()
+        if choice == 4:
+            DeleteWeapon()
+        if choice == 5:
+            ModifyWeapon()
+        if choice == 6:
+            ViewShoppingCart()
+        choice = menu()
     
